76adfa2b33bc450c707cd234e9dcabb06f8a79ac
omkarmundhe46 <omkarmundhe46@gmail.com>
2025-07-10 12:42:52 +0530

    Added Entity / Tables

---
+++ src/main/java/com/ecom/entities/Testimonial.java
@@ -0,0 +1,24 @@
+package com.ecom.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Testimonial {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String message;
+  private String pic;
+  private boolean active;
+
+}
