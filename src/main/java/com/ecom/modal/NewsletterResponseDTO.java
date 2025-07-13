cc8ac7fce27dc4b9f2591e5f2cf6078e615ee811
omkarmundhe46 <omkarmundhe46@gmail.com>
2025-07-13 10:15:45 +0530

    Added ResponseDTO Layers

---
+++ src/main/java/com/ecom/modal/NewsletterResponseDTO.java
@@ -0,0 +1,17 @@
+package com.ecom.modal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewsletterResponseDTO {
+  private Long id;
+  private String email;
+  private boolean active;
+
+}
